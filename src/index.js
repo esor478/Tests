5a08cc72737756eb5a0191129c95dbc5b79f7525
esor478 <74151719+esor478@users.noreply.github.com>
2022-09-19 14:51:37 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Hello world</h1>
+    <p1>This can work</p1>
+  </div>,
+  document.getElementById("root")
+);
